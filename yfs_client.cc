a528aa03ac1daee63a5e2c57d5ef4f659419e06a
Richard <richard@Richards-MacBook-Pro.local>
2019-11-07 14:35:22 +0800

    Lab1 Part2B

--- yfs_client.cc
+++ yfs_client.cc
@@ -130,7 +130,50 @@ yfs_client::setattr(inum ino, size_t size)
      * note: get the content of inode ino, and modify its content
      * according to the size (<, =, or >) content length.
      */
-
+    printf("setattr: job started\n");
+    extent_protocol::attr inode_attributes;
+    if (ec->getattr(ino, inode_attributes) != extent_protocol::OK) {
+        printf("setattr: failed to get attributes\n");
+        r = IOERR;
+        return r;
+    }
+    if (inode_attributes.type == 0) {
+        printf("setattr: inode number is invalid\n");
+        r = IOERR;
+        return r;
+    }
+    std::string data;
+    if (ec->get(ino, data) == extent_protocol::OK) {
+        printf("setattr: inode data fetched\n");
+        if (data.size() > size) {
+            printf("setattr: shrinking data\n");
+            data = data.substr(0, size);
+        }
+        else if (data.size() < size) {
+            printf("setattr: expanding data\n");
+            for (size_t i = 0; i < size - data.size(); i++) {
+                data = data + "\0";
+            }
+        }
+        else {
+            printf("setattr: no need to change data\n");
+        }
+        printf("setattr: writing back data\n");
+        if (ec->put(ino, data) == extent_protocol::OK) {
+            printf("setattr: done writing\n");
+        }
+        else {
+            printf("setattr: failed to write back data\n");
+            r = IOERR;
+            return r;
+        }
+    }
+    else {
+        printf("setattr: failed to fetch data\n");
+        r = IOERR;
+        return r;
+    }
+    printf("setattr: job done\n");
     return r;
 }
 
@@ -144,52 +187,53 @@ yfs_client::create(inum parent, const char *name, mode_t mode, inum &ino_out)
      * note: lookup is what you need to check if file exist;
      * after create file or dir, you must remember to modify the parent infomation.
      */
+    printf("create: job started\n");
     bool found;
     if (lookup(parent, name, found, ino_out) != extent_protocol::OK) {
         if (found) {
-            printf("create: file already exists");
+            printf("create: file already exists\n");
             r = EXIST;
             return r;
         }
         else {
-            printf("create: something wrong with lookup");
+            printf("create: something wrong with lookup\n");
             r = IOERR;
             return r;
         }
     }
     else {
-        printf("create: file is ok to be created");
+        printf("create: file is ok to be created\n");
     }
     if (ec->create(extent_protocol::T_FILE, ino_out) != extent_protocol::OK) {
         r = IOERR;
-        printf("create: failed to create file");
+        printf("create: failed to create file\n");
         return r;
     }
     else {
-        printf("create: file created");
+        printf("create: file created\n");
         std::string parent_entries;
         if (ec->get(parent, parent_entries) == extent_protocol::OK) {
-            printf("create: updating parent");
+            printf("create: updating parent\n");
             parent_entries.append(name);
             parent_entries.append("/");
             parent_entries.append(filename(ino_out));
             parent_entries.append("/");
             if (ec->put(parent, parent_entries) == extent_protocol::OK) {
-                printf("create: parent updated");
+                printf("create: parent updated\n");
             }
             else {
                 r = IOERR;
-                printf("create: failed to update parent");
+                printf("create: failed to update parent\n");
                 return r;
             }
         }
         else {
             r = IOERR;
-            printf("create: failed to read parent");
+            printf("create: failed to read parent\n");
             return r;
         }
     }
-    printf("create: job finished");
+    printf("create: job done\n");
     return r;
 }
 
@@ -217,6 +261,7 @@ yfs_client::lookup(inum parent, const char *name, bool &found, inum &ino_out)
      * note: lookup file from parent dir according to name;
      * you should design the format of directory content.
      */
+    printf("lookup: job started\n");
     std::list<dirent> dir_entries;
     if (readdir(parent, dir_entries) == extent_protocol::OK) {
         printf("lookup: readdir returned OK\n");
@@ -224,17 +269,17 @@ yfs_client::lookup(inum parent, const char *name, bool &found, inum &ino_out)
         for (it = dir_entries.begin(); it != dir_entries.end(); it++) {
             printf("look up: extract entry -> name:%s, inum: %llu\n", it->name.c_str(), it->inum);
             if (!it->name.compare(name)) {
-                printf("lookup: got match");
+                printf("lookup: got match\n");
                 found = true;
                 ino_out = it->inum;
                 r = EXIST;
-                printf("lookup: job finished");
+                printf("lookup: job done\n");
                 return r;
             }
         }
         found = false;
-        printf("lookup: no match");
-        printf("lookup: job finished");
+        printf("lookup: no match\n");
+        printf("lookup: job done\n");
     }
     else {
         printf("lookup: readdir didn't return OK\n");
@@ -255,6 +300,7 @@ yfs_client::readdir(inum dir, std::list<dirent> &list)
      * and push the dirents to the list.
      */
     // 获取dir这个inode的属性。
+    printf("readdir: job started\n");
     extent_protocol::attr dir_attributes;
     ec->getattr(dir, dir_attributes);
     // 检查这个inode是不是目录。
@@ -264,12 +310,12 @@ yfs_client::readdir(inum dir, std::list<dirent> &list)
         return r;
     }
     else {
-        printf("readdir: inode is a dir: %s\n");
+        printf("readdir: inode is a dir\n");
         std::string dir_entries;
         ec->get(dir, dir_entries);
         printf("readdir: got dir entries: %s\n", dir_entries.c_str());
         std::string entry_name, entry_inum;
-        int former_slash = 0, latter_slash = 0;
+        unsigned int former_slash = 0, latter_slash = 0;
         while (former_slash < dir_entries.size()) {
             // 从前一个/开始找下一个/，二者之间的先是name后是inum，应该是成对出现的。
             latter_slash = dir_entries.find('/', former_slash);
@@ -286,7 +332,7 @@ yfs_client::readdir(inum dir, std::list<dirent> &list)
             list.push_back(dir_entry);
         }
     }
-    printf("readdir: job finished");
+    printf("readdir: job done\n");
     return r;
 }
 
@@ -299,7 +345,39 @@ yfs_client::read(inum ino, size_t size, off_t off, std::string &data)
      * your code goes here.
      * note: read using ec->get().
      */
-
+    printf("read: job started\n");
+    extent_protocol::attr inode_attributes;
+    if (ec->getattr(ino, inode_attributes) != extent_protocol::OK) {
+        printf("read: failed to get attributes\n");
+        r = IOERR;
+        return r;
+    }
+    if (inode_attributes.type == 0) {
+        printf("read: inode number is invalid\n");
+        r = IOERR;
+        return r;
+    }
+    if (off >= inode_attributes.size) {
+        printf("read: offset is larger than size\n");
+        r = IOERR;
+        return r;
+    }
+    std::string inode_data;
+    if (ec->get(ino, inode_data) != extent_protocol::OK) {
+        printf("read: failed to read data\n");
+        r = IOERR;
+        return r;
+    }
+    size_t remaining = inode_attributes.size - off;
+    if (remaining >= size) {
+        printf("read: size is ok\n");
+        data = inode_data.substr(off, size);
+    }
+    else {
+        printf("read: read exceeds the size of file\n");
+        data = inode_data.substr(off, remaining);
+    }
+    printf("read: job done\n");
     return r;
 }
 
@@ -314,7 +392,67 @@ yfs_client::write(inum ino, size_t size, off_t off, const char *data,
      * note: write using ec->put().
      * when off > length of original file, fill the holes with '\0'.
      */
-
+    printf("write: job started\n");
+    extent_protocol::attr inode_attributes;
+    if (ec->getattr(ino, inode_attributes) != extent_protocol::OK) {
+        printf("write: failed to get attributes\n");
+        r = IOERR;
+        return r;
+    }
+    if (inode_attributes.type == 0) {
+        printf("write: inode number is invalid\n");
+        r = IOERR;
+        return r;
+    }
+    std::string inode_data;
+    if (ec->get(ino, inode_data) != extent_protocol::OK) {
+        printf("write: failed to read data\n");
+        r = IOERR;
+        return r;
+    }
+    bool needsfill = false;
+    if (off > inode_attributes.size) {
+        printf("write: offset larger than size\n");
+        needsfill = true;
+    }
+    std::string to_write = "";
+    for (size_t i = 0; i < size; i++) {
+        to_write = to_write + data[i];
+    }
+    if (needsfill) {
+        printf("write: preparing data with gap\n");
+        std::string gap(off - inode_attributes.size, '\0');
+        inode_data.append(gap);
+        inode_data.append(to_write);
+        bytes_written = size + off - inode_attributes.size;
+    }
+    else if (off + size < inode_attributes.size) {
+        printf("write: preparing data with only middle part updated\n");
+        std::string former_data, latter_data;
+        former_data = inode_data.substr(0, off);
+        latter_data = inode_data.substr(off + size, inode_attributes.size);
+        former_data.append(to_write);
+        former_data.append(latter_data);
+        inode_data = former_data;
+        bytes_written = size;
+    }
+    else {
+        printf("write: preparing data with the right part updated\n");
+        std::string former_data;
+        former_data = inode_data.substr(0, off);
+        former_data.append(to_write);
+        inode_data = former_data;
+        bytes_written = size;
+    }
+    if (ec->put(ino, inode_data) == extent_protocol::OK) {
+        printf("write: writing success\n");
+    }
+    else {
+        printf("write: failed to write new inode data\n");
+        r = IOERR;
+        return r;
+    }
+    printf("write: job done\n");
     return r;
 }
 
